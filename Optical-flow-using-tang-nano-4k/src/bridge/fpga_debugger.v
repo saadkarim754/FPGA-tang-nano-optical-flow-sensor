30814fdd44bcabdae764b6bff583d16485b1bf77
saadkarim754 <saadkarim754@gmail.com>
2026-04-15 22:54:43 +0500

    more-debugging

--- Optical-flow-using-tang-nano-4k/src/bridge/fpga_debugger.v
+++ Optical-flow-using-tang-nano-4k/src/bridge/fpga_debugger.v
@@ -30,22 +30,22 @@ module fpga_debugger (
         char_rom[4]  = "A"; char_rom[5]  = "]"; char_rom[6]  = " "; char_rom[7]  = "B";
         char_rom[8]  = "o"; char_rom[9]  = "o"; char_rom[10] = "t"; char_rom[11] = "i"; 
         char_rom[12] = "n"; char_rom[13] = "g"; char_rom[14] = "."; char_rom[15] = ".";
-        char_rom[16] = "."; char_rom[17] = "\r"; char_rom[18] = "\n"; char_rom[19] = 0;
+        char_rom[16] = "."; char_rom[17] = 8'h0D; char_rom[18] = 8'h0A; char_rom[19] = 0;
         
         // Msg 1 (Start idx 32)
         char_rom[32] = "["; char_rom[33] = "F"; char_rom[34] = "P"; char_rom[35] = "G";
         char_rom[36] = "A"; char_rom[37] = "]"; char_rom[38] = " "; char_rom[39] = "O";
         char_rom[40] = "V"; char_rom[41] = "2"; char_rom[42] = "6"; char_rom[43] = "4";
         char_rom[44] = "0"; char_rom[45] = " "; char_rom[46] = "O"; char_rom[47] = "K";
-        char_rom[48] = "!"; char_rom[49] = "\r"; char_rom[50] = "\n"; char_rom[51] = 0;
+        char_rom[48] = "!"; char_rom[49] = 8'h0D; char_rom[50] = 8'h0A; char_rom[51] = 0;
         
         // Msg 2 (Start idx 64)
         char_rom[64] = "["; char_rom[65] = "F"; char_rom[66] = "P"; char_rom[67] = "G";
         char_rom[68] = "A"; char_rom[69] = "]"; char_rom[70] = " "; char_rom[71] = "M";
         char_rom[72] = "C"; char_rom[73] = "U"; char_rom[74] = " "; char_rom[75] = "T";
         char_rom[76] = "A"; char_rom[77] = "K"; char_rom[78] = "E"; char_rom[79] = "O";
-        char_rom[80] = "V"; char_rom[81] = "E"; char_rom[82] = "R"; char_rom[83] = "\r";
-        char_rom[84] = "\n"; char_rom[85] = 0;
+        char_rom[80] = "V"; char_rom[81] = "E"; char_rom[82] = "R"; char_rom[83] = 8'h0D;
+        char_rom[84] = 8'h0A; char_rom[85] = 0;
     end
 
     localparam S_START        = 0;
@@ -66,6 +66,7 @@ module fpga_debugger (
 
     // Give some time on start for PuTTY to catch up
     reg [23:0] startup_delay;
+    reg [25:0] frame_timeout;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
@@ -75,6 +76,7 @@ module fpga_debugger (
             tx_req          <= 1'b0;
             handover_to_mcu <= 1'b0;
             startup_delay   <= 24'd0;
+            frame_timeout   <= 26'd0;
         end else begin
             case (state)
                 S_START: begin
@@ -118,10 +120,12 @@ module fpga_debugger (
                 end
                 
                 S_WAIT_FRAME: begin
-                    if (first_frame_seen) begin
+                    if (first_frame_seen || frame_timeout == 26'd54_000_000) begin
                         state    <= S_PRINT_DONE;
                         msg_base <= 7'd64;
                         rom_ptr  <= 7'd64;
+                    end else begin
+                        frame_timeout <= frame_timeout + 1'b1;
                     end
                 end
                 
